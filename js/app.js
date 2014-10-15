72e2a1a04b1809b0152dc107a0dce401bc79612c
respectYourNeighbour <philip_luca@yahoo.com>
2014-10-15 12:46:31 +0300

    added component in app.js for adding active class to links

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ App.Router.map(function() {
 	this.route('about');
 	this.route('contact');
 });
+
+
+//Component for applying active class on links;
+App.LinkLiComponent = Em.Component.extend({
+  tagName: 'li',
+  classNameBindings: ['active'],
+  active: function() {
+    return this.get('childViews').anyBy('active');
+  }.property('childViews.@each.active')
+});
