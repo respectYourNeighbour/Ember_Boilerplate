e80c64eda2c7c57f48f4d5e492f3fc9aa511f371
respectYourNeighbour <philip_luca@yahoo.com>
2014-10-16 09:54:10 +0300

    added new controllers, a image, and some functionality

--- js/app.js
+++ js/app.js
@@ -8,6 +8,21 @@ App.Router.map(function() {
 	this.route('contact');
 });
 
+App.IndexController = Ember.Controller.extend({
+	productsCount: 3
+});
+
+App.AboutController = Ember.Controller.extend({
+	numeAbout: 'Hi, from the inside of the About Controller.',
+	logo: 'image/logo.png'
+});
+
+App.ContactController = Ember.Controller.extend({
+	time: function () {
+		return(new Date()).toDateString()
+	}.property()
+});
+
 
 //Component for applying active class on links;
 App.LinkLiComponent = Em.Component.extend({
